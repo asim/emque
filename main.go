32256d358bb995e993f8ae2127e8809775ae2380
Asim Aslam <asim@chuhnk.me>
2016-12-18 17:18:23 +0000

    remove Log func

--- main.go
+++ main.go
@@ -44,13 +44,6 @@ func init() {
 	}
 }
 
-func Log(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
-		handler.ServeHTTP(w, r)
-	})
-}
-
 func (m *mq) pub(topic string, payload []byte) error {
 	if *proxy {
 		return m.client.Publish(topic, payload)
