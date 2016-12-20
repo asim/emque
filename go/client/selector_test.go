52490f667f553980a3fe952dd1fc718f9a67c506
Asim Aslam <asim@chuhnk.me>
2016-12-20 17:28:05 +0000

    selector test

---
+++ go/client/selector_test.go
@@ -0,0 +1,53 @@
+package client
+
+import (
+	"testing"
+)
+
+func TestSelectAll(t *testing.T) {
+	testServers := []string{"a", "b", "c"}
+	sa := new(SelectAll)
+	if err := sa.Set(testServers...); err != nil {
+		t.Fatal(err)
+	}
+	servers, err := sa.Get("test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, tserver := range testServers {
+		var seen bool
+		for _, server := range servers {
+			if server == tserver {
+				seen = true
+				break
+			}
+		}
+		if !seen {
+			t.Fatal("did not find", tserver)
+		}
+	}
+}
+
+func TestSelectShard(t *testing.T) {
+	testServers := []string{"a", "b", "c"}
+	ss := new(SelectShard)
+	if err := ss.Set(testServers...); err != nil {
+		t.Fatal(err)
+	}
+	servers, err := ss.Get("test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	var seen bool
+	for _, tserver := range testServers {
+		for _, server := range servers {
+			if server == tserver {
+				seen = true
+				break
+			}
+		}
+	}
+	if !seen {
+		t.Fatal("did not find any test servers")
+	}
+}
