f5ada4d479892c29dea49e306cdb3bc72489c07d
Hegyesi Dániel <d.hegyesi@gmail.com>
2017-12-08 19:08:29 +0100

    Replace deprecated Upgrade function

--- server/http/handler.go
+++ server/http/handler.go
@@ -9,6 +9,14 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
 func pub(w http.ResponseWriter, r *http.Request) {
 	topic := r.URL.Query().Get("topic")
 
@@ -27,15 +35,14 @@ func pub(w http.ResponseWriter, r *http.Request) {
 func sub(w http.ResponseWriter, r *http.Request) {
 	var wr writer
 
-	if u := r.Header.Get("Upgrade"); len(u) == 0 || u != "websocket" {
-		wr = &httpWriter{w}
-	} else {
-		conn, err := websocket.Upgrade(w, r, w.Header(), 1024, 1024)
+	if websocket.IsWebSocketUpgrade(r) {
+		conn, err := upgrader.Upgrade(w, r, w.Header())
 		if err != nil {
-			http.Error(w, fmt.Sprintf("Could not open websocket connection: %v", err), http.StatusBadRequest)
 			return
 		}
 		wr = &wsWriter{conn}
+	} else {
+		wr = &httpWriter{w}
 	}
 
 	topic := r.URL.Query().Get("topic")
